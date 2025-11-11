8348a5d47a22c57d7100a752568a78d8c0bd5eaa
eblanchard <someone@anything.com>
2025-11-10 19:09:57 -0600

    testcom

---
+++ .venv/app.py
@@ -0,0 +1,137 @@
+import utilities as util
+import yara
+import os
+from tqdm import tqdm
+import psutil
+from halo import Halo
+import sys
+import atexit
+import keyboard
+from io import StringIO
+def main():
+    
+    
+    global saved_state_flag
+    global high_water_mark_file
+    high_water_mark_file = " " 
+    saved_state_flag = False
+    atexit.register(save_progress_on_exit)
+    message1 ='''
+        Welcome, What would you like to do? Enter the number for your choice.
+                          
+        1) Scan the file system.
+        2) Scan the active running processes.
+        3) Scan the entire system.
+
+'''
+    message2 = '''
+
+         Welcome, What would you like to do? Enter the number for your choice.
+                          
+        1) Scan the file system.
+        2) Scan the active running processes.
+        3) Scan the entire system.
+        4) Continue with an in progress scan                 
+                          
+                           
+                             '''
+    if os.path.exists('scan_saved_progress_file'):
+        with open("scan_saved_progress_file", "r") as f:
+            high_water_mark_file = f.readline()
+        greet_user(message2)
+    else:
+         greet_user(message1)
+    
+    
+def greet_user(message):    
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print("Welcome to CompSci 150 Malware Scanner")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    print(" ")
+    
+    user_response = input(message)
+     
+                    
+    if user_response == '1':
+            scan_file_system()
+    elif user_response == '2':
+            scan_processes()
+    elif user_response == '3':
+            scan_file_system()
+            scan_processes()
+    elif user_response == '4':
+           saved_state_flag = True
+           scan_file_system()
+    else:
+            print("That selection does not exist")
+
+
+def scan_file_system():
+    global saved_state_flag
+         
+    with Halo(text="Gathering file system file data. Once scanning begins, press escape to exit.", spinner='dots', text_color = 'magenta', animation = 'bounce'):
+        compiled_rules = util.get_compiled_rules()
+        files_to_scan = util.get_all_files()
+        if saved_state_flag == True:
+             if high_water_mark_file in files_to_scan:
+                  high_water_mark_index = files_to_scan.index(high_water_mark_file)
+                  temporary_file_list = files_to_scan[high_water_mark_index:]
+                  files_to_scan = temporary_file_list
+                  saved_state_flag = False
+        files_to_scan = tqdm(files_to_scan)
+    matches = []
+    for file in files_to_scan:
+        high_water_mark_file = file
+        if keyboard.is_pressed('esc'):
+             sys.exit()
+        base_file = os.path.basename(file)
+        message = "Scanning file: " + str(base_file[:15]).ljust(20)
+        files_to_scan.set_description(message)
+        try:
+            matches = compiled_rules.match(file, console_callback=error_handler)
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            pass
+    for match in matches:
+        for item in match:
+            print(str(item))
+
+
+
+def scan_processes():
+    compiled_rules = util.get_compiled_rules()
+    pids = util.get_process_pids()
+    for id in tqdm(pids):
+            matches = compiled_rules.match(pid=id)
+
+def save_progress_on_exit():
+
+    line_to_write = high_water_mark_file
+    with open("scan_saved_progress_file", "w") as f:
+         f.write(line_to_write)
+
+
+def error_handler(message):
+     if "An error occurred: could not open file" in message:
+          return 
+     else:
+          print(message)
+
+if __name__ == "__main__":
+    main()
+
+def positive_yara_detection(file_path):
+     file_hash = calculate_file_hash(file_path)
+     is_file_vt_verified_bad = util.confirm_yara_positive(file_hash)
+     if is_file_vt_verified_bad:
+          # DELETE THE FILE
---
+++ .venv/utilities.py
@@ -0,0 +1,57 @@
+import os
+import psutil
+import yara
+from halo import Halo
+
+def get_yara_rules_paths(yara_directory):
+    """
+    Load YARA rules from the 'yara_rules' directory.
+
+    Returns:
+        list: A dictionary of YARA rule file paths.
+   """
+    yara_files = {}
+    i = 1
+    for root, _, files in os.walk(yara_directory):
+        for file in files:
+            if file.endswith(".yar"):
+                yara_files["yara_rule" + str(i)] = (os.path.join(root, file))
+                i += 1
+    return yara_files
+
+
+def get_all_files():
+    """
+    Load YARA rules from the 'yara_rules' directory.
+
+    Returns:
+        list: A list of all the files on a computer
+   """
+    partitions = psutil.disk_partition(all=False)
+    all_files_list = []
+    for p in partitions:
+        for root, _, files in os.walk(p.device):
+            for file in files:
+                all_files_list.append(os.path.join(root, file))
+    return all_files_list
+
+
+def get_process_pids():
+    return psutil.pids()
+
+def get_compiled_rules():
+    spinner = Halo(text='Compiling Yara Rules', spinner='dots')
+    spinner.text_color = 'blue'
+    spinner.start()
+    
+
+    if os.path.exists('.//yararules//yara_compiled_rules'):
+        compiled_rules = yara.load('.//yararules//yara_compiled_rules')
+    
+
+    else:    
+        yara_rules_dict = get_yara_rules_paths('.\\yararules')
+        compiled_rules = yara.compile(filepaths=yara_rules_dict)
+        compiled_rules.save('.//yararules//yara_compiled_rules')
+    spinner.stop()
+    return compiled_rules
